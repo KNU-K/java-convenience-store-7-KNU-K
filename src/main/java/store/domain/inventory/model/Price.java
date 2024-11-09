5252bd13b67ce2d6605b9d7d3ed8d3299600f9d7
KNU-K <knukang334@gmail.com>
2024-11-10 02:24:20 +0900

    feat(Price): 가격에 대한 도메인을 형성합니다.

---
+++ src/main/java/store/domain/inventory/model/Price.java
@@ -0,0 +1,45 @@
+package store.domain.inventory.model;
+
+import java.text.DecimalFormat;
+
+public class Price {
+
+    public static final Price ZERO = Price.of(0);
+    private static final String NUMBER_FORMAT = "#,###";
+
+    private final int amount;
+
+    public Price(int amount) {
+        this.amount = amount;
+    }
+
+    public static Price of(int amount) {
+        return new Price(amount);
+    }
+
+    @Override
+    public String toString() {
+        return formatAmount(amount);
+    }
+
+    public Price multiply(int quantity) {
+        return new Price(amount * quantity);
+    }
+
+    public Price applyPercentage(int percentage) {
+        return Price.of((int) (amount * (percentage / 100.0)));
+    }
+
+    public Price plus(Price other) {
+        return Price.of(this.amount + other.amount);
+    }
+
+    public Price subtract(Price other) {
+        return Price.of(this.amount - other.amount);
+    }
+
+    private String formatAmount(int amount) {
+        return new DecimalFormat(NUMBER_FORMAT).format(amount);
+    }
+
+}
