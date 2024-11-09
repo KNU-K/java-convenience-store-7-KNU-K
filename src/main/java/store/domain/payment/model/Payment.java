3279b4103265096082fa05d97f8b974d6d299e40
KNU-K <knukang334@gmail.com>
2024-11-10 02:23:35 +0900

    feat(Payment): 결제에 대한 도메인을 형성합니다.

---
+++ src/main/java/store/domain/payment/model/Payment.java
@@ -0,0 +1,15 @@
+package store.domain.payment.model;
+
+import store.domain.inventory.model.Price;
+
+import java.util.List;
+
+/**
+ * @param paymentItems            결제 항목 리스트
+ * @param totalPrice              결제 총액
+ * @param membershipDiscountPrice 할인 총액
+ * @param promotionDiscountPrice
+ */
+public record Payment(List<PaymentItem> paymentItems, Price totalPrice, Price membershipDiscountPrice,
+                      Price promotionDiscountPrice) {
+}
