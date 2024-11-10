ab96ba2210bc81612d6f69a75e463b3ce130a27c
KNU-K <knukang334@gmail.com>
2024-11-10 18:37:36 +0900

    feat(initializer): InventoryInitializer 구성(싱글 톤 패턴 추가)
    - 싱글톤 패턴을 통해서 초기에 설정을 로드할 때에 싱글 톤으로 접근하기 위함
    
    - 테스트 시 싱글톤을 초기화 하는 함수 구성

---
+++ src/main/java/store/common/initializer/InventoryInitializer.java
@@ -0,0 +1,28 @@
+package store.common.initializer;
+
+import store.domain.inventory.model.Inventory;
+
+public class InventoryInitializer {
+    private static InventoryInitializer instance;
+    private Inventory inventory;
+
+    private InventoryInitializer() {
+        inventory = new Inventory(); // 초기화
+    }
+
+    public static InventoryInitializer getInstance() {
+        if (instance == null) {
+            instance = new InventoryInitializer();
+        }
+        return instance;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void resetInventory() {
+        // 테스트 후 상태를 초기화
+        this.inventory = new Inventory();
+    }
+}
