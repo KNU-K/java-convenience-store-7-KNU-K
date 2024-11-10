ab841f4927d570ba9281d9d50d4e9a34914a0690
KNU-K <knukang334@gmail.com>
2024-11-10 18:33:13 +0900

    config(constant): FileConstants 추가 (재고, 프로모션 파일 위치)
    이후 전처리 해야할 파일 경로를 담고 있음.

---
+++ src/main/java/store/common/constants/FileConstants.java
@@ -0,0 +1,7 @@
+package store.common.constants;
+
+public class FileConstants {
+    public static final String PROMOTIONS_FILE_PATH = "promotions.md";
+    public static final String PRODUCTS_FILE_PATH = "products.md";
+}
+
