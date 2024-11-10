00a9de0e28d70eb239f6a1a85ae5e54c0f93a1c7
KNU-K <knukang334@gmail.com>
2024-11-10 18:42:09 +0900

    feat(payment): Payment를 구성하는 컨트롤러 및 서비스 추가
    - Payment 는 Order 레이어에서 검증을 통해서 수행된 작업을 결제하는 작업을 한다.

---
+++ src/main/java/store/domain/payment/controller/PaymentController.java
@@ -0,0 +1,40 @@
+package store.domain.payment.controller;
+
+import store.common.controller.BaseController;
+import store.common.view.InputView;
+import store.common.view.OutputView;
+import store.domain.inventory.service.InventoryService;
+import store.domain.order.model.Order;
+import store.domain.order.model.OrderItem;
+import store.domain.payment.factory.ReceiptFactory;
+import store.domain.payment.model.Payment;
+import store.domain.payment.model.Receipt;
+import store.domain.payment.service.PaymentService;
+
+import java.util.List;
+
+public class PaymentController extends BaseController {
+    private final PaymentService paymentService;
+    private final InventoryService inventoryService;
+
+    public PaymentController(InputView inputView, OutputView outputView, PaymentService paymentService, InventoryService inventoryService) {
+        super(inputView, outputView);
+        this.paymentService = paymentService;
+        this.inventoryService = inventoryService;
+    }
+
+    public void doPayment(Order order) {
+        boolean isMembership = inputView.confirmMembership();
+        Payment payment = paymentService.processPayment(order, isMembership);
+        inventoryService.decreaseStockOfItem(order);
+        Receipt receipt = ReceiptFactory.createReceipt(payment);
+        outputView.displayMessage(receipt.toString());
+    }
+
+
+    public boolean askForAdditionalPurchaseAfterPayment() {
+        return inputView.getContinueShoppingConfirmation();
+    }
+
+
+}
---
+++ src/main/java/store/domain/payment/service/PaymentService.java
@@ -0,0 +1,70 @@
+package store.domain.payment.service;
+
+import store.domain.inventory.model.Price;
+import store.domain.order.model.Order;
+import store.domain.order.model.OrderItem;
+import store.domain.payment.model.Payment;
+import store.domain.payment.model.PaymentItem;
+import store.domain.promotion.model.PromotionPolicy;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class PaymentService {
+
+    private static final int MEMBERSHIP_DISCOUNT_PERCENTAGE = 30;
+    private static final Price NO_DISCOUNT = Price.ZERO;
+
+    public Payment processPayment(Order order, boolean isMembership) {
+        List<PaymentItem> paymentItems = order.streamOrderItems().map(processPaymentForEachItem()).toList();
+
+        Price totalGiftDiscount = calculateTotalGiftDiscount(order);
+        if (!isMembership) {
+            return new Payment(paymentItems, order.totalPrice(), NO_DISCOUNT, totalGiftDiscount);
+        }
+
+        Price membershipDiscount = calculateDiscountForMembership(order);
+        return new Payment(paymentItems, order.totalPrice(), membershipDiscount, totalGiftDiscount);
+    }
+
+    private Function<OrderItem, PaymentItem> processPaymentForEachItem() {
+        return orderItem -> new PaymentItem(orderItem.name(), orderItem.quantity(), calculateGiftItemsCount(orderItem), orderItem.price(), calculateGiftDiscount(orderItem));
+    }
+
+    private int calculateGiftItemsCount(OrderItem orderItem) {
+        PromotionPolicy promotionPolicy = orderItem.promotionPolicy();
+        if (promotionPolicy != null) {
+            return promotionPolicy.getGiftQuantity(orderItem.promotionQuantity());
+        }
+        return 0;
+    }
+
+    private Price calculateGiftDiscount(OrderItem orderItem) {
+        int giftItemsCount = calculateGiftItemsCount(orderItem);
+        return orderItem.price().multiply(giftItemsCount);
+    }
+
+    private Price calculateTotalGiftDiscount(Order order) {
+        return order.streamOrderItems().map(this::calculateGiftDiscount).reduce(NO_DISCOUNT, Price::plus);
+    }
+
+    private Price calculateEachGiftItemPrice(OrderItem orderItem) {
+        if (orderItem.promotionPolicy() == null) {
+            return Price.of(0);
+        }
+        int quantity = orderItem.promotionPolicy().getApplicableQuantityOfGift(orderItem.quantity());
+        return orderItem.price().multiply(quantity);
+    }
+
+    private Price calculateTotalGiftItemsPrice(Order order) {
+        Price a =  order.streamOrderItems().map(this::calculateEachGiftItemPrice)
+                .reduce(NO_DISCOUNT, Price::plus);
+        return a;
+    }
+
+    private Price calculateDiscountForMembership(Order order) {
+        return order.totalPrice()
+                .subtract(calculateTotalGiftItemsPrice(order))
+                .applyPercentage(MEMBERSHIP_DISCOUNT_PERCENTAGE);
+    }
+}
