3bbdf07ed70fc0f7f9c087028a5bb3aa4195fdf7
KNU-K <knukang334@gmail.com>
2024-11-10 18:43:11 +0900

    feat(Order): Order 도메인을 구성하는 컨트롤러 및 서비스 추가
    - Order는 장바구니 담기와, 결제 사이의 검증 역할을 수행하는 도메인으로써, Cart를 통해서 결제를 위한 주문을 생성한다.

---
+++ src/main/java/store/domain/order/controller/OrderController.java
@@ -0,0 +1,72 @@
+package store.domain.order.controller;
+
+import store.common.controller.BaseController;
+import store.common.view.InputView;
+import store.common.view.OutputView;
+import store.domain.inventory.model.Cart;
+import store.domain.inventory.model.CartItem;
+import store.domain.inventory.model.Price;
+import store.domain.order.model.Order;
+import store.domain.order.model.OrderItem;
+import store.domain.order.service.OrderService;
+import store.domain.promotion.model.PromotionPolicy;
+
+import java.util.List;
+
+public class OrderController extends BaseController {
+    private final OrderService orderService;
+
+    public OrderController(InputView inputView, OutputView outputView, OrderService orderService) {
+        super(inputView, outputView);
+        this.orderService = orderService;
+    }
+
+    public Order placeOrder(Cart cart) {
+        List<OrderItem> orderItems = createOrderItems(cart);
+        Price totalPrice = orderService.calculateTotalCartPrice(orderItems);
+        return new Order(orderItems, totalPrice);
+    }
+
+    private List<OrderItem> createOrderItems(Cart cart) {
+        return cart.streamItem()
+                .map(this::createOrderItemWithPromotion)
+                .map(this::confirmAndAdjustRegularPriceQuantity)
+                .toList();
+    }
+
+    private OrderItem createOrderItemWithPromotion(CartItem cartItem) {
+        PromotionPolicy policy = orderService.getPromotionPolicy(cartItem);
+        int availablePromotionQuantity = orderService.calculateAvailablePromotionQuantity(cartItem);
+        Price price = orderService.findProductPrice(cartItem.name());
+        if (policy == null) {
+            return new OrderItem(cartItem, price, 0, null);
+        }
+        if (orderService.isEligibleForExtraItem(cartItem) && confirmExtraPromotion(cartItem)) {
+            cartItem.increaseQuantity();
+        }
+        return new OrderItem(cartItem, price, Math.min(cartItem.quantity(), availablePromotionQuantity), policy);
+    }
+    private OrderItem confirmAndAdjustRegularPriceQuantity(OrderItem orderItem) {
+        if (orderItem.promotionQuantity() == 0) return orderItem;
+        int applicableQuantity=orderService.getApplicableQuantity(orderItem);
+        int remainingQuantity = orderItem.quantity() - applicableQuantity;
+        if (remainingQuantity <= 0) remainingQuantity = orderItem.quantity();
+        boolean confirmRegularPrice = confirmRegularPriceOption(orderItem, remainingQuantity);
+        orderService.checkSufficientQuantity(orderItem.name(), orderItem.quantity());
+        if (!confirmRegularPrice) {
+            orderItem.decreaseQuantity(remainingQuantity);
+            orderItem.updatePromotionQuantity(applicableQuantity);
+        }
+        return orderItem;
+    }
+
+
+
+    private boolean confirmExtraPromotion(CartItem cartItem) {
+        return inputView.confirmExtraPromotion(cartItem.name());
+    }
+
+    private boolean confirmRegularPriceOption(OrderItem cartItem, int remainingQuantity) {
+        return inputView.confirmRegularPriceOption(cartItem.name(), remainingQuantity);
+    }
+}
---
+++ src/main/java/store/domain/order/service/OrderService.java
@@ -0,0 +1,62 @@
+package store.domain.order.service;
+
+import store.domain.inventory.model.CartItem;
+import store.domain.inventory.model.Price;
+import store.domain.inventory.service.InventoryService;
+import store.domain.order.model.OrderItem;
+import store.domain.promotion.model.PromotionPolicy;
+import store.domain.promotion.service.PromotionService;
+
+import java.util.List;
+
+public class OrderService {
+    private final InventoryService inventoryService;
+    private final PromotionService promotionService;
+
+    public OrderService(InventoryService inventoryService, PromotionService promotionService) {
+        this.inventoryService = inventoryService;
+        this.promotionService = promotionService;
+    }
+
+    public int calculateAvailablePromotionQuantity(CartItem cartItem) {
+        if (!isPromotionEligible(cartItem)) {
+            return 0;
+        }
+        return getAvailablePromotionStock(cartItem.name());
+
+    }
+
+    public PromotionPolicy getPromotionPolicy(CartItem cartItem) {
+        return promotionService.getPromotionPolicy(cartItem);
+    }
+
+    public Price calculateTotalCartPrice(List<OrderItem> orderItems) {
+        return orderItems.stream()
+                .map(inventoryService::getTotalPriceOfEachItem)
+                .reduce(Price.ZERO, Price::plus);
+    }
+
+    public Price findProductPrice(String productName) {
+        return inventoryService.getProductPrice(productName);
+    }
+
+    public int getAvailablePromotionStock(String productName) {
+        return inventoryService.getPromotionStockQuantity(productName);
+    }
+
+    public boolean isPromotionEligible(CartItem item) {
+        return promotionService.isPromotionApplicable(item);
+    }
+
+    public boolean isEligibleForExtraItem(CartItem item) {
+        return inventoryService.isPromotionQuantitySufficient(item) && promotionService.isEligibleForExtraItem(item);
+    }
+
+    public void checkSufficientQuantity(String name, int quantity) {
+        inventoryService.checkSufficientQuantity(name,quantity);
+    }
+
+    public int getApplicableQuantity(OrderItem orderItem) {
+        return inventoryService.getApplicableQuantity(orderItem);
+    }
+}
