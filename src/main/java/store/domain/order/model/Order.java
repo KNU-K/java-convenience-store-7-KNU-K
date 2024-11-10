c5d1ba6e1715eda29483509707762e326f76f664
KNU-K <knukang334@gmail.com>
2024-11-10 18:29:20 +0900

    feat(Order): 주문에 대한 도메인을 형성합니다.

--- src/main/java/store/domain/order/model/Order.java
+++ src/main/java/store/domain/order/model/Order.java
@@ -3,11 +3,19 @@ package store.domain.order.model;
 import store.domain.inventory.model.Price;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 
-public record Order(List<OrderItem> orderItems, Price totalPrice, boolean isMembership) {
+public final class Order {
+    private final List<OrderItem> orderItems;
+    private Price totalPrice;
+
+    public Order(List<OrderItem> orderItems, Price totalPrice) {
+        this.orderItems = orderItems;
+        this.totalPrice = totalPrice;
+    }
 
     public Stream<OrderItem> streamOrderItems() {
         return orderItems.stream();
@@ -16,5 +24,35 @@ public record Order(List<OrderItem> orderItems, Price totalPrice, boolean isMemb
     public void forEachItems(Consumer<OrderItem> action) {
         orderItems.forEach(action);
     }
+
+    public List<OrderItem> orderItems() {
+        return orderItems;
+    }
+
+    public Price totalPrice() {
+        return totalPrice;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Order) obj;
+        return Objects.equals(this.orderItems, that.orderItems) &&
+                Objects.equals(this.totalPrice, that.totalPrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderItems, totalPrice);
+    }
+
+    @Override
+    public String toString() {
+        return "Order[" +
+                "orderItems=" + orderItems + ", " +
+                "totalPrice=" + totalPrice + ']';
+    }
+
 
 }
--- src/main/java/store/domain/order/model/OrderItem.java
+++ src/main/java/store/domain/order/model/OrderItem.java
@@ -1,17 +1,91 @@
 package store.domain.order.model;
 
-import store.domain.inventory.model.Cart;
 import store.domain.inventory.model.CartItem;
 import store.domain.inventory.model.Price;
 import store.domain.promotion.model.PromotionPolicy;
 
-import java.util.List;
+import java.util.Objects;
 
-public record OrderItem(String name, Price price, int quantity, int promotionQuantity,
-                        PromotionPolicy promotionPolicy) {
+public final class OrderItem {
+    private final String name;
+    private final Price price;
+    private final PromotionPolicy promotionPolicy;
+    private int quantity;
+    private int promotionQuantity;
+
+    public OrderItem(String name, Price price, int quantity, int promotionQuantity,
+                     PromotionPolicy promotionPolicy) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotionQuantity = promotionQuantity;
+        this.promotionPolicy = promotionPolicy;
+    }
 
     public OrderItem(CartItem item, Price price, int promotionQuantity, PromotionPolicy promotionPolicy) {
         this(item.name(), price, item.quantity(), promotionQuantity, promotionPolicy);
     }
 
+    public void increaseQuantity() {
+        this.quantity += 1;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Price price() {
+        return price;
+    }
+
+    public int quantity() {
+        return quantity;
+    }
+
+    public int promotionQuantity() {
+        return promotionQuantity;
+    }
+
+    public PromotionPolicy promotionPolicy() {
+        return promotionPolicy;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (OrderItem) obj;
+        return Objects.equals(this.name, that.name) &&
+                Objects.equals(this.price, that.price) &&
+                this.quantity == that.quantity &&
+                this.promotionQuantity == that.promotionQuantity &&
+                Objects.equals(this.promotionPolicy, that.promotionPolicy);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, quantity, promotionQuantity, promotionPolicy);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem[" +
+                "name=" + name + ", " +
+                "price=" + price + ", " +
+                "quantity=" + quantity + ", " +
+                "promotionQuantity=" + promotionQuantity + ", " +
+                "promotionPolicy=" + promotionPolicy + ']';
+    }
+
+    public void decreaseQuantity(int remainingQuantity) {
+        quantity -= remainingQuantity;
+    }
+
+    public void resetPromotionQuantity() {
+        promotionQuantity =0;
+    }
+
+    public void updatePromotionQuantity(int applicableQuantity) {
+        promotionQuantity = applicableQuantity;
+    }
 }
