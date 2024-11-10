6ada92a1913d9a6fb822a91a33f3d647ada57e2f
KNU-K <knukang334@gmail.com>
2024-11-11 08:18:48 +0900

    refactor(utility): 변수 네이밍을 가독성있고 확실하게 변경

--- src/main/java/store/common/utility/FileReader.java
+++ src/main/java/store/common/utility/FileReader.java
@@ -1,6 +1,8 @@
 package store.common.utility;
 
 import store.Application;
+import store.common.exception.ErrorMessages;
+import store.common.exception.InvalidFileException;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -14,18 +16,19 @@ public class FileReader {
 
     private static final Charset CHARSET = StandardCharsets.UTF_8;
     private static final String DELIMITER = ",";
+    private static final int HEADER_SKIP = 1;
 
     public static List<String[]> readMarkdownFile(String fileName) {
         try (InputStream inputStream = Application.class.getClassLoader().getResourceAsStream(fileName);
              BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(inputStream), CHARSET))) {
 
             return reader.lines()
-                    .skip(1)
+                    .skip(HEADER_SKIP)
                     .map(line -> line.split(DELIMITER))
                     .toList();
 
         } catch (Exception e) {
-            throw new RuntimeException("Failed to read file: " + fileName, e);
+            throw new InvalidFileException(ErrorMessages.INVALID_FILE);
         }
     }
 }
