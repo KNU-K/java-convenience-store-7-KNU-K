ecab5283c7cf87c68c01206bb6cd0f14be378c9f
KNU-K <knukang334@gmail.com>
2024-11-11 18:17:54 +0900

    feat(utility): DateTimeGenerator 구성
    고정된 시간을 다루기 위해, 싱글톤으로 구성된 DateTimeGenerator 구성

---
+++ src/main/java/store/common/utility/DateTimeGenerator.java
@@ -0,0 +1,38 @@
+package store.common.utility;
+
+import camp.nextstep.edu.missionutils.DateTimes;
+
+import java.time.LocalDate;
+
+public class DateTimeGenerator {
+    private static DateTimeGenerator instance;
+    private boolean isFixedClockEnabled = false;
+    private LocalDate fixedDate;
+    private DateTimeGenerator() {
+    }
+
+    // 3. 인스턴스를 반환하는 public 메서드 (Lazy initialization)
+    public static synchronized DateTimeGenerator getInstance() {
+        if (instance == null) {
+            instance = new DateTimeGenerator();
+        }
+        return instance;
+    }
+
+    // 예시로 날짜를 반환하는 메서드
+    public LocalDate now() {
+        if(isFixedClockEnabled) {
+            return fixedDate;
+        }
+        return DateTimes.now().toLocalDate();
+    }
+    public void enableFixedClockMode(LocalDate fixedDate) {
+        this.fixedDate = fixedDate;
+        this.isFixedClockEnabled = true;
+    }
+
+    // 고정 클럭 모드를 비활성화하는 메서드
+    public void disableFixedClockMode() {
+        this.isFixedClockEnabled = false;
+    }
+}
